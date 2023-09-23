7786a48a2b27de273bf274d0335e5d4ddda5ef31
Leticia Speda <leticiaspeda@gmail.com>
2023-09-23 18:56:58 -0300

    feat: adicionado navigation bar com titulo e botao para navegacao entre telas

--- MomondoScreenTrip/MomondoScreenTrip/ViewController.swift
+++ MomondoScreenTrip/MomondoScreenTrip/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .purple
+        view.backgroundColor = #colorLiteral(red: 0.1174478158, green: 0.02265601605, blue: 0.2253929079, alpha: 1)
+        configureStyle()
     }
-
-
+    
+    @objc func handleBack() {
+        print("Ok")
+    }
+    
+    private func configureStyle() {
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = #colorLiteral(red: 0.1174478158, green: 0.02265601605, blue: 0.2253929079, alpha: 1)
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        navigationItem.title = "Malta"
+        navigationItem.leftBarButtonItem = .init(image: .init(systemName: "chevron.left"), style: .plain, target: self, action: #selector(handleBack))
+        navigationController?.navigationBar.tintColor = .white
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+    }
+    
 }
 
