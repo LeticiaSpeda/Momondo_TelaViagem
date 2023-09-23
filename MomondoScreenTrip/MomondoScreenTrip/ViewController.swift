a62854828b280dc9b767567fdb573bcdb8bbdfcf
Leticia Speda <leticiaspeda@gmail.com>
2023-09-23 18:12:30 -0300

    build: configuracoes iniciais

---
+++ MomondoScreenTrip/MomondoScreenTrip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MomondoScreenTrip
+//
+//  Created by Leticia Speda on 23/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .purple
+    }
+
+
+}
+
