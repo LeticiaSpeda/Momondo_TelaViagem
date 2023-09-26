1f4c8812acdd321d7bc1843e66505d5ed0cb518a
Leticia Speda <leticiaspeda@gmail.com>
2023-09-26 11:13:29 -0300

    feat: adicionado nova forma de adicionar imagem atraves do ponto

--- MomondoScreenTrip/MomondoScreenTrip/Cell/TripCollectionViewCell.swift
+++ MomondoScreenTrip/MomondoScreenTrip/Cell/TripCollectionViewCell.swift
@@ -4,7 +4,7 @@ final class tripCollectionViewCell: UICollectionViewCell {
     static let identifier = String(describing: tripCollectionViewCell.self)
     
     private lazy var tripImage: UIImageView = {
-        let img = UIImage(named: "maldivas")
+        let img = UIImage.maldivas
         let view = UIImageView(image: img)
         view.clipsToBounds = true
         view.layer.cornerRadius = 10
@@ -14,11 +14,16 @@ final class tripCollectionViewCell: UICollectionViewCell {
     }()
     
     init() {
+        configureHierarchy()
         configureContraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureHierarchy() {
+        contentView.addSubview(tripImage)
     }
     
     private func configureContraints() {
